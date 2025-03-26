6c9eb632fc594f6534d8fe90e655db4458e26a9c
Arpit Soni <arpitjsoni@Arpits-MacBook-Air.local>
2025-03-26 22:11:06 +0530

    Fixed the url

--- src/urls/index.tsx
+++ src/urls/index.tsx
@@ -1,2 +1,1 @@
-export const API_BASE_URL = 'http://localhost:4000'
-//export const API_BASE_URL = 'https://json-server-five-sand.vercel.app/'
+export const API_BASE_URL = 'https://json-server-five-sand.vercel.app/'
