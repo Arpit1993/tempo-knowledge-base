c5718742c16562a426933532828faa6fe9fe6903
Arpit Soni <arpitjsoni@Arpits-MacBook-Air.local>
2025-03-28 14:52:00 +0530

    fixed teh center pane width

--- src/components/CenterPane.tsx
+++ src/components/CenterPane.tsx
@@ -4,7 +4,7 @@ const CenterPaneContainer = styled.div`
   position: relative;
   left: 280px;
   padding: 120px;
-  width: 1200px;
+  width: 700px;
 `;
 export const CenterPane = ({ children }: { children: any }) => {
   return <CenterPaneContainer>{children}</CenterPaneContainer>;
